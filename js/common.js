8e5f5fa1a568240767da0cb93e95a9b45f8956ea
RobTheCradle <1353961893@qq.com>
2019-05-05 22:51:14 +0800

    init

---
+++ js/common.js
@@ -0,0 +1,23 @@
+$(function(){
+	
+	resizeWindowViewer($("body"));
+	
+})
+
+//监听浏览器窗口变化
+$(window).resize(function () { 
+	
+	resizeWindowViewer($("body"))	
+})
+
+//重置窗口高宽
+function resizeWindowViewer($ele){
+	
+	let windowWidth = $(window).width();
+	let windowHeight = $(window).height();
+	
+	console.log("windowWidth="+windowWidth+"px;windowHeight="+windowHeight+"px;");
+	
+	$ele.width(windowWidth);
+	$ele.height(windowHeight);
+}
